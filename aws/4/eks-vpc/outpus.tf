48f860d317692c84528bcb0032a4782b2064824c
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-06 14:15:40 +0200

    adding vpc default sg output

--- aws/4/eks-vpc/outpus.tf
+++ aws/4/eks-vpc/outpus.tf
@@ -9,3 +9,7 @@ output "private_subnet_id" {
 output "public_subnet_id" {
   value = [for az, subnet in aws_subnet.public : subnet.id]
 }
+
+output "default_security_group_id" {
+  value = aws_vpc.network.default_security_group_id
+}

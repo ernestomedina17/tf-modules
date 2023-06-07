f95814f9cee064757565ac62492a24a5fe498e4e
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-07 23:29:15 +0200

    bugfix

--- aws/4/eks-vpc/main.tf
+++ aws/4/eks-vpc/main.tf
@@ -83,8 +83,8 @@ resource "aws_default_route_table" "private" {
 resource "aws_route" "private" {
   route_table_id         = aws_default_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  #nat_gateway_id         = aws_nat_gateway.private.id
-  #depends_on             = [aws_nat_gateway.private]
+  nat_gateway_id         = aws_nat_gateway.public.id
+  depends_on             = [aws_nat_gateway.public]
 }
 
 resource "aws_route_table_association" "private" {

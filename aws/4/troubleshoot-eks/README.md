91956e3966a34ca05735641ab8f4e5a6e7980558
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-07 17:19:22 +0200

    troubleshoot eks iam role module

---
+++ aws/4/troubleshoot-eks/README.md
@@ -0,0 +1,1 @@
+https://docs.aws.amazon.com/systems-manager-automation-runbooks/latest/userguide/automation-awssupport-troubleshooteksworkernode.html
---
+++ aws/4/troubleshoot-eks/main.tf
@@ -0,0 +1,59 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = loca.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+      "iam:GetInstanceProfile",
+      "iam:ListAttachedRolePolicies",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNatGateways",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeImages",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeInstances",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeInstanceAttribute",
+      "eks:DescribeCluster",
+      "ssm:DescribeInstanceInformation",
+      "ssm:ListCommandInvocations",
+      "ssm:ListCommands",
+      "ssm:SendCommand",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = local.name
+  description = local.name
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}

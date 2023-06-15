9a5ed1339a7d860d3da4f886035dd2f1bcde5948
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-15 12:43:53 +0200

    eks cluster sg

---
+++ aws/4/eks-cluster-sg/main.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "eks_cluster" {
+  name        = "eks-cluster-${var.name}"
+  description = "https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "eks-cluster-sg-${var.name}-15062023"
+    "kubernetes.io/cluster/${var.name}" = "owned"
+    "aws:eks:cluster-name" = var.name
+  }
+}
+
+# TO-DO: Enhance Security 
+resource "aws_security_group_rule" "default" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true 
+  security_group_id = aws_security_group.eks_cluster.id
+}
+
+resource "aws_security_group_rule" "default" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = aws_security_group.eks_cluster.id
+}
---
+++ aws/4/eks-cluster-sg/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "Name of the cluster role"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}

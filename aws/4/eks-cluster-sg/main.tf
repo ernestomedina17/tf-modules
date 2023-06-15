a0dbce13f4085e8106ad0951ce9b04dbdb17b170
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-15 13:04:02 +0200

    test tag

--- aws/4/eks-cluster-sg/main.tf
+++ aws/4/eks-cluster-sg/main.tf
@@ -4,9 +4,9 @@ resource "aws_security_group" "eks_cluster" {
   vpc_id      = var.vpc_id
 
   tags = {
-    Name = "eks-cluster-sg-${var.name}-15062023"
+    Name                                = "eks-cluster-sg-${var.name}-15062023"
     "kubernetes.io/cluster/${var.name}" = "owned"
-    "aws:eks:cluster-name" = var.name
+    "aws:eks\\:cluster-name"            = var.name
   }
 }
 
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "ingress_default" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  self              = true 
+  self              = true
   security_group_id = aws_security_group.eks_cluster.id
 }
 
@@ -25,7 +25,7 @@ resource "aws_security_group_rule" "egress_default" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks      = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.eks_cluster.id
 }

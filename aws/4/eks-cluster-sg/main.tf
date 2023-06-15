73e53e2a9987f7091c20006d59b22da1466bba3a
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-15 13:24:08 +0200

    removing tags, aws will add them

--- aws/4/eks-cluster-sg/main.tf
+++ aws/4/eks-cluster-sg/main.tf
@@ -2,14 +2,6 @@ resource "aws_security_group" "eks_cluster" {
   name        = "eks-cluster-${var.name}"
   description = "https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html"
   vpc_id      = var.vpc_id
-
-  # Needs to be added manually: aws:eks:cluster-name as AWS rejects the double colons. 
-  tags = {
-    Name                                = "eks-cluster-sg-${var.name}-15062023"
-    "kubernetes.io/cluster/${var.name}" = "owned"
-    "aws:eks:cluster-name"              = var.name
-    #"aws\u003Aeks\u003Acluster-name"    = var.name
-  }
 }
 
 # TO-DO: Enhance Security 

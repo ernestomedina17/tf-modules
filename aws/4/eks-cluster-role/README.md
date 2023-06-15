e3eea5700a027ba61dc69ffc78b6233d281417c8
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-15 11:51:58 +0200

    iam cluster role module

---
+++ aws/4/eks-cluster-role/README.md
@@ -0,0 +1,1 @@
+Created the IAM Cluster Role
---
+++ aws/4/eks-cluster-role/main.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "cluster" {
+  name               = "eks-cluster-${var.name}"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+# Optionally, enable Security Groups for Pods
+# https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.cluster.name
+}
